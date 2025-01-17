30f7dd2d6e7b562d4258d561031776b83809ff85
atik112 <atik112@gmail.com>
2025-01-17 13:48:54 +0300

    Update Otto_ardu.ino

--- Otto_ardu.ino
+++ Otto_ardu.ino
@@ -55,23 +55,6 @@ void avoidObstacle() {
   }
 }
 
-void wander() {
-  Otto.walk(5, 1000, 1);  // Walk for distance 5 units, 1000 ms duration, speed 1
-  
-  // Randomly decide to turn Left or Right
-  if (rand() % 2 == 0) {
-    Otto.turn(3, 1000, 1);
-  } else {
-    Otto.turn(3, 1000, -1);
-  }
-}
-
-void get_back() {
-   Otto.sing(S_surprise);
-   Otto.walk(2, 1000, -1); // Move backward
-   Otto.turn(3, 1000, 1);  // Turn left
-}
-
 void handleSerialCommands() {
   if (Serial.available() > 0) {
     String receivedData = Serial.readStringUntil('\n'); // Read till newline
@@ -85,69 +68,46 @@ void handleSerialCommands() {
     // Handle different commands
     if (receivedData == "shake_leg") {
       Otto.shakeLeg(1, 1500, 1);
-      Otto.home();
     } else if (receivedData == "dance") {
       Otto.moonwalker(3, 1000, 25, 1);
       Otto.sing(S_superHappy);
-      Otto.home();
     } else if (receivedData == "walk_forward") {
       Otto.walk(2, 1000, 1);
-      Otto.home();
+      Serial.print("walk");
     } else if (receivedData == "walk_backward") {
       Otto.sing(S_OhOoh);
       Otto.walk(2, 1000, -1); // Move backward
-      Otto.home();
     } else if (receivedData == "turn_left") {
       Otto.turn(2, 1000, 1); // Turn left
-      Otto.home();
     } else if (receivedData == "turn_right") {
       Otto.turn(2, 1000, -1); // Turn right
-      Otto.home();
     } else if (receivedData == "bend") {
       Otto.bend(1, 500, 1); // Bend gesture
       Otto.sing(S_happy);
-      Otto.home();
     } else if (receivedData == "moonwalker") {
       Otto.moonwalker(3, 1000, 25, 1); // Perform moonwalker dance
     } else if (receivedData == "jump") {
       Otto.jump(1, 500); // Perform a jump (simulated)
-      Otto.home();
     } else if (receivedData == "OttoHappy") {
       Otto.playGesture(OttoHappy);
-      Otto.home();
     } else if (receivedData == "OttoSuperHappy") {
       Otto.playGesture(OttoSuperHappy);
-      Otto.home();
     } else if (receivedData == "OttoSad") {
       Otto.playGesture(OttoSad);
-      Otto.home();
     } else if (receivedData == "fart") {
       Otto.playGesture(OttoFart);
-      Otto.home();
     } else if (receivedData == "Frightened") {
       Otto.playGesture(OttoFail);
-      Otto.home();
     } else if (receivedData == "Angry") {
       Otto.playGesture(OttoAngry);
-      Otto.home();
     } else if (receivedData == "Confused") {
       Otto.playGesture(OttoConfused);
-      Otto.home();
     } else if (receivedData == "Love") {
       Otto.playGesture(OttoLove);
-      Otto.home();
     } else if (receivedData == "Fretful") {
       Otto.playGesture(OttoFretful);
-      Otto.home();
     } else if (receivedData == "magic") {
       Otto.playGesture(OttoMagic);
-      Otto.home();
-    } else if (receivedData == "wander"){
-      wander();
-      Otto.home();
-    } else if (receivedData == "get_back"){
-      get_back();
-      Otto.home();
     }
      else {
       Serial.println("Unknown command: " + receivedData);
@@ -156,6 +116,5 @@ void handleSerialCommands() {
 }
 
 void loop() {
-  avoidObstacle(); // Check for obstacles and react
   handleSerialCommands(); // Check for incoming serial commands
 }
